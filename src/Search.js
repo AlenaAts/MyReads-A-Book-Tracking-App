ace80671fb59a85cf2c22c0b854f978683278612
Alena <amongstthestones@gmail.com>
2018-07-23 17:57:05 +0300

    added updateBooks method for search

--- src/Search.js
+++ src/Search.js
@@ -1,7 +1,47 @@
 import React from 'react'
+import Book from './Book'
+import * as BooksAPI from './BooksAPI'
+import escapeRegExp from 'escape-string-regexp'
 
 class Search extends React.Component {
+	state = {
+		query: '',
+		books: []
+	}
+
+	updateQuery = (query) => {
+		this.setState({
+			query: query
+		})
+		this.updateBooks(query);
+	}
+
+	updateBooks = (query) => {
+		/*BooksAPI.search(query).then((books) => {
+			this.setState({ books: books })
+		})
+
+		*/
+		if (query) {
+		BooksAPI.search(query).then((books) => {
+			this.setState({ books: books })
+		})
+		} else {
+			this.setState({ books: [] })
+		}
+		
+	}
+
 	render() {
+		/*let showingBooks
+
+		if (this.state.query) {
+			const match = new RegExp(escapeRegExp(this.state.query), 'i')
+			showingBooks = this.state.books.filter((book) => match.test(book.name))
+		} else {
+			showingBooks = this.state.books
+		}*/
+
 		return(
 			<div className="search-books">
 	            <div className="search-books-bar">
@@ -16,12 +56,25 @@ class Search extends React.Component {
 			                  However, remember that the BooksAPI.search method DOES search by title or author. So, don't worry if
 			                  you don't find a specific author or title. Every search is limited by search terms.
 			                */}
-			                <input type="text" placeholder="Search by title or author"/>
+			                <input 
+			                type="text"
+			                placeholder="Search by title or author"
+			                value={this.state.query}
+			                onChange={(event) => this.updateQuery(event.target.value)}
+			                />
 		              	</div>
 	            </div>
 
 	            <div className="search-books-results">
-	              	<ol className="books-grid"></ol>
+	              	<ol className="books-grid">
+	              	{this.state.books.map(book => (
+	              			<li key={this.state.books.id}>
+	              				<Book
+	              					book={this.state.books}
+	              				/>
+	              			</li>
+	              		))}
+	              	</ol>
 	            </div>
           </div>
 		)
